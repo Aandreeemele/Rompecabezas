69b58d8dcae86d04ad474e55d1289e5a15db7b57
Aandreeemele <aamelendez@scl.edu.gt>
2025-02-06 18:29:11 -0600

    Version header terminada, darle unicamente detalles

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-import { cargarCartas } from "./componentes/tablero/tablero.js"
-cargarCartas();
+import { crearBotones } from "./componentes/header/header.js";
+import { cargarCartas } from "./componentes/tablero/tablero.js";
 
 let DOM = document.querySelector("#root");
-
 
 let contenedor = document.createElement('div');
 contenedor.className = "contenedor";
 
-
 let header = document.createElement('div');
-header.className = "div-header"
-contenedor.appendChild(header)
+header.className = "div-header";
+contenedor.appendChild(header);
 
+crearBotones(header);
 
 let progreso = document.createElement('div');
-progreso.className = "div-progreso"
-contenedor.appendChild(progreso)
-
+progreso.className = "div-progreso";
+contenedor.appendChild(progreso);
 
 let tablero = document.createElement('div');
-tablero.className = "div-tablero"
+tablero.className = "div-tablero";
 tablero.appendChild(cargarCartas());
-contenedor.appendChild(tablero)
-
+contenedor.appendChild(tablero);
 
 let footer = document.createElement('div');
-footer.className = "div-footer"
-contenedor.appendChild(footer)
+footer.className = "div-footer";
+contenedor.appendChild(footer);
 
 
-
-
-DOM.appendChild(contenedor)
-
-
+DOM.appendChild(contenedor);
