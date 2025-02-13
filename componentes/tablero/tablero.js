579fa878e914064412a7420ceab2ebb0c38621e3
Aandreeemele <aamelendez@scl.edu.gt>
2025-02-13 13:33:13 -0600

    Version Animacion de cartas

--- componentes/tablero/tablero.js
+++ componentes/tablero/tablero.js
@@ -1,22 +1,64 @@
-import { todas_las_cartas } from "./data.js" 
+import { todas_las_cartas } from "./data.js";
 
-function item(contenido){
-    let div = document.createElement('div');
-    div.innerText = contenido;
-    div.className = "carta"
+let cartasVolteadas = [];
+let parejasEncontradas = 0;
+
+function item(contenido) {
+    let div = document.createElement("div");
+    div.className = "carta";
+    div.dataset.valor = contenido;
+
+    let frente = document.createElement("div");
+    frente.className = "frente";
+    frente.textContent = contenido; 
+
+    let dorso = document.createElement("div");
+    dorso.className = "dorso";
+    dorso.textContent = "";
+
+    div.appendChild(frente);
+    div.appendChild(dorso);
+
+    div.addEventListener("click", () => manejarVolteo(div));
 
     return div;
 }
 
-function cargarCartas() {
-    let div = document.createElement('div');
-    div.className = ("div-tablero");
+function manejarVolteo(carta) {
+    if (cartasVolteadas.length < 2 && !carta.classList.contains("volteada")) {
+        carta.classList.add("volteada");
+        cartasVolteadas.push(carta);
 
-    todas_las_cartas.forEach((letra)=>{
+        if (cartasVolteadas.length === 2) {
+            setTimeout(verificarPareja, 1000);
+        }
+    }
+}
+
+function verificarPareja() {
+    let [carta1, carta2] = cartasVolteadas;
+
+    if (carta1.dataset.valor === carta2.dataset.valor) {
+        parejasEncontradas++;
+    } else {
+        carta1.classList.remove("volteada");
+        carta2.classList.remove("volteada");
+    }
+
+    cartasVolteadas = [];
+}
+
+function cargarCartas() {
+    let div = document.createElement("div");
+    div.className = "div-tablero";
+
+    todas_las_cartas.sort(() => Math.random() - 0.5); // Mezclar cartas
+
+    todas_las_cartas.forEach((letra) => {
         div.appendChild(item(letra));
     });
 
     return div;
 }
 
-export{cargarCartas}
+export { cargarCartas };
