9f37a50685d34500cc8de636e31eaf099cf302ed
Aandreeemele <aamelendez@scl.edu.gt>
2025-02-18 22:09:24 -0600

    Estructura de carpetas actualizada 2.0

--- componentes/footer/footer.js
+++ componentes/footer/footer.js
@@ -2,7 +2,6 @@ function cargarFooter() {
     const root = document.getElementById("root");
     let footer = document.createElement("div");
     footer.className = "div-footer";
-    footer.textContent = "© 2025 - Mi Juego de Cartas";
   
     root.appendChild(footer);
   }
--- componentes/progreso/progreso.js
+++ componentes/progreso/progreso.js
@@ -2,7 +2,6 @@ function cargarProgreso() {
     const root = document.getElementById("root");
     let progreso = document.createElement("div");
     progreso.className = "div-progreso";
-    progreso.textContent = "Progreso: 0%";
   
     root.appendChild(progreso);
   }
